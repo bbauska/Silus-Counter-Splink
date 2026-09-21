88c3d4d0ecb7d9606a16ed90d8de565e07db1f1b
Brian Bauska <brianbauska@gmail.com>
2026-09-20 17:20:07 -0700

    Clean up comments in scripts.js
    Remove outdated comments and update counter initialization.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /* /js/scripts.js in github Silus-Counter making silus-counter-4.bauska.org */
 /* Jan 1, 2026 = xx,000 */
 let counter = 0;
-/* zz,000 Jan 1, 2026
+/* 5,000 Sep 18, 2026 Friday night 
   all times are approximate. */
 
 function count() {
